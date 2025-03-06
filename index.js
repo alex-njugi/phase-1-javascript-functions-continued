35035a2d06387048063cab934d5b351848d04dea
alex njugi karanja <alex.karanja4@student.moringaschool.com>
2025-03-06 13:08:44 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  
+  const mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+  };
+  
+  function wrapAdjective(flair = "*") {
+    return function(adjective = "special") {
+      return `You are ${flair}${adjective}${flair}!`;
+    };
+  }
+  
+  console.log(saturdayFun()); 
+  console.log(saturdayFun("go hiking")); 
+  
+  console.log(mondayWork()); 
+  console.log(mondayWork("work from home")); 
+  
+  const encouragingPromptFunction = wrapAdjective("!!!");
+  console.log(encouragingPromptFunction("amazing")); 
+
+  const simpleWrap = wrapAdjective("||");
+  console.log(simpleWrap("a hard worker")); 
+  console.log(simpleWrap()); 
+  
